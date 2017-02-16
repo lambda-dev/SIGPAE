fb6b3373318daa636751f34cb6a94933e9d0641a
Adolfo Jeritson <adolfojeritson@gmail.com>
2017-02-16 17:01:45 -0400

    Sprint #1 Hist 1-2-3-5

---
+++ SIGPAE/historia1/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.model_form_upload, name='model_form_upload'),
+    url(r'^a$',views.some,name='some'),
+]
---
+++ SIGPAE/historia1/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .forms import DocumentForm
+from readpdf import *
+import os
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def after_upload(request):
+    return render(request,'historia1/after_upload.html')
+
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            filename, file_extension = os.path.splitext('documents/'+str(request.FILES['document']))
+            if file_extension != ".pdf":
+                form1 = DocumentForm()
+                return render(request,'historia1/model_form_error.html', {
+                    'form' : form1})
+            strng =leer('documents/'+str(request.FILES['document']))
+            return render(request,'historia1/some.html', {
+                'strng' : strng})
+    else:
+        form = DocumentForm()
+        #strng =leer(str(request.FILES['document']))
+    return render(request,'historia1/model_form_upload.html', {
+        'form' : form
+    })
+
+def some(request):
+    return render(request,'historia1/some.html')
